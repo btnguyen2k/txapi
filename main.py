598ca316fabce840722f61594c9a470750f801a3
Thanh Nguyen <thanhn@microsoft.com>
2023-07-02 22:02:16 +1000

    add api_split_text

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 from fastapi import FastAPI
 from pydantic import BaseModel
 
@@ -16,6 +17,10 @@ tags_metadata = [
             "description": "OpenAI's embeddings API",
             "url": "https://platform.openai.com/docs/api-reference/embeddings",
         },
+    },
+    {
+        "name": "split_text",
+        "description": "Split input text to smaller chunks",
     },
     {
         "name": "token_counts",
@@ -39,9 +44,11 @@ app = FastAPI(
     },
 )
 
+#----------------------------------------------------------------------#
+
 import models
 
-api_resp_no_model = {
+api_resp_no_model: dict = {
     "error": {
         "message": "The specified model does not exist.",
         "type": "invalid_request_error",
@@ -49,7 +56,7 @@ api_resp_no_model = {
     }
 }
 
-api_resp_input_too_long = {
+api_resp_input_too_long: dict = {
     "error": {
         "message": "Input exceeds model's maximum length.",
         "type": "invalid_request_error",
@@ -62,7 +69,13 @@ api_resp_input_too_long = {
     }
 }
 
-api_resp_embeddings = {
+#----------------------------------------------------------------------#
+
+class EmbeddingsRequest(BaseModel):
+    model: str # name of the model used to extract embeddings
+    input: str # the input string to extract embeddings
+
+api_resp_embeddings: dict = {
   "object": "list",
   "data": [
     {
@@ -78,15 +91,16 @@ api_resp_embeddings = {
   }
 }
 
-class EmbeddingsRequest(BaseModel):
-    model: str
-    input: str
-
 @app.post("/embeddings", tags=["embeddings"])
 async def api_embeddings(req: EmbeddingsRequest):
+    """
+    api_embeddings takes an input string and return the embedding vector of the input.
+
+    This API supports offline models only!
+    """
     model_name = req.model
     model_meta = models.hf_model_metadata(model_name)
-    if model_meta == None:
+    if not bool(model_meta):
         return api_resp_no_model
     if len(req.input) > model_meta["max_input_length"]:
         resp = api_resp_input_too_long.copy()
@@ -94,8 +108,8 @@ async def api_embeddings(req: EmbeddingsRequest):
         resp["error"]["meta"]["model"] = model_name
         return resp
 
-    model = models.get_model(model_name)
-    tokenizer = models.get_tokenizer(model_name)
+    model = models.hf_get_model(model_name)
+    tokenizer = models.hf_get_tokenizer(model_name)
     embeddings = models.encode_embeddings(model, tokenizer, [req.input])
     resp = api_resp_embeddings.copy()
     resp["model"] = model_name
@@ -103,11 +117,104 @@ async def api_embeddings(req: EmbeddingsRequest):
     resp["meta"] = model_meta
     return resp
 
-class TokenCountsRequest(BaseModel):
-    model: str
-    input: str
+#----------------------------------------------------------------------#
 
-api_resp_token_counts = {
+class SplitTextRequest(BaseModel):
+    model: str              # name of the model used to count tokens
+    input: str              # the input text to be split
+    type: str = 'plaintext' # type of the input text, default to 'plaintext'
+    max_tokens: int = 0     # max chunk size, in tokens
+    chunk_overlap: int = 0  # specify how many tokens chunks can overlap with each other
+
+api_resp_split_text: dict = {
+    "object": "list",
+    "data": [
+        {
+            "object": "string",
+            "chunk": "<placeholder>",
+            "index": 0
+        }
+    ],
+    "model": "<placeholder>",
+}
+
+@app.post("/split_text", tags=["split_text"])
+async def api_split_text(req: SplitTextRequest):
+    hf_model = True
+    model_name = req.model
+    model_meta = models.hf_model_metadata(model_name)
+    if not bool(model_meta):
+        hf_model = False
+        model_meta = models.openai_model_metadata(model_name)
+        if not bool(model_meta):
+            return api_resp_no_model
+
+    max_tokens = req.max_tokens
+    if max_tokens <= 0 or max_tokens > model_meta['max_tokens']:
+        max_tokens = model_meta['max_tokens']
+    elif max_tokens < model_meta['min_tokens']:
+        max_tokens = model_meta['min_tokens']
+    chunk_overlap = req.chunk_overlap
+    if chunk_overlap <= 0:
+        chunk_overlap = 0
+
+    if hf_model:
+        tokenizer = models.hf_get_tokenizer(model_name)
+        def length_func(input_text)->int:
+            encoded_input = tokenizer(input_text, padding=True)
+            return len(encoded_input["input_ids"])
+        length_function = length_func
+    else:
+        def length_func(input_text)->int:
+            return models.openai_token_counts(model_name, input_text)
+        length_function = length_func
+
+    req.type = req.type.lower()
+    req.input = req.input.replace("\r", "\n")
+
+    if req.type == "md" or req.type == "markdown":
+        from langchain.text_splitter import MarkdownHeaderTextSplitter
+        headers_to_split_on = [
+            ("#", "H1"),
+            ("##", "H2"),
+            ("###", "H3"),
+            ("####", "H4"),
+            ("#####", "H5"),
+            ("######", "H6"),
+        ]
+        markdown_splitter = MarkdownHeaderTextSplitter(headers_to_split_on=headers_to_split_on)
+        md_chunks = markdown_splitter.split_text(req.input)
+        chunks = []
+        for md_chunk in md_chunks:
+            text = md_chunk.page_content
+            if md_chunk.metadata:
+                for header in reversed(headers_to_split_on):
+                    if header[1] in md_chunk.metadata:
+                        text = header[0]+" "+md_chunk.metadata[header[1]] + "\n\n" + text
+            chunks += models.split_text(text.strip(), length_function, max_tokens, chunk_overlap)
+    else:
+        chunks = models.split_text(req.input, length_function, max_tokens, chunk_overlap)
+
+    resp = api_resp_split_text.copy()
+    resp["model"] = model_name
+    resp["meta"] = model_meta
+    resp["data"] = []
+    for i in range(len(chunks)):
+        resp["data"].append({
+            "object": "string",
+            "chunk": chunks[i],
+            "index": i,
+            "chunk_size": length_func(chunks[i])
+        })
+    return resp
+
+#----------------------------------------------------------------------#
+
+class TokenCountsRequest(BaseModel):
+    model: str # name of the model used to extract embeddings
+    input: str # the input string to extract embeddings
+
+api_resp_token_counts: dict = {
   "object": "list",
   "data": [
     {
@@ -124,11 +231,11 @@ async def api_token_counts(req: TokenCountsRequest):
     hf_model = True
     model_name = req.model
     model_meta = models.hf_model_metadata(model_name)
-    if model_meta == None:
+    if not bool(model_meta):
         hf_model = False
         model_meta = models.openai_model_metadata(model_name)
-    if model_meta == None:
-        return api_resp_no_model
+        if not bool(model_meta):
+            return api_resp_no_model
     if len(req.input) > model_meta["max_input_length"]:
         resp = api_resp_input_too_long.copy()
         resp["error"]["meta"] = model_meta
@@ -136,7 +243,7 @@ async def api_token_counts(req: TokenCountsRequest):
         return resp
 
     if hf_model:
-        tokenizer = models.get_tokenizer(model_name)
+        tokenizer = models.hf_get_tokenizer(model_name)
         encoded_input = tokenizer(req.input, padding=True)
         token_counts = len(encoded_input["input_ids"])
     else:
@@ -148,12 +255,24 @@ async def api_token_counts(req: TokenCountsRequest):
     resp["meta"] = model_meta
     return resp
 
+#----------------------------------------------------------------------#
+
 @app.get("/health", tags=["health"])
 async def health():
+    """
+    health is the health check endpoint.
+    :return:
+    """
     return "ok"
+
+#----------------------------------------------------------------------#
 
 import uvicorn
 
 if __name__ == "__main__":
-    #uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=4)
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    num_workers = int(os.getenv("NUM_WORKERS", 1))
+    if num_workers < 1:
+        uvicorn.run(app, host="0.0.0.0", port=8000)
+    else:
+        uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=num_workers)
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import torch.nn.functional as F
 import tiktoken
+import re
 
 model_alias = {
     "multi-qa-mpnet-base-cos-v1": "sentence-transformers/multi-qa-mpnet-base-cos-v1",
@@ -14,57 +15,69 @@ model_alias = {
 ## OpenAI ##
 openai_models_meta = {
     # chat
-    "gpt-4": {"max_tokens": 8192, "max_input_length": 8192*6},
-    "gpt-4-32k": {"max_tokens": 32768, "max_input_length": 32768*6},
-    "gpt-3.5-turbo": {"max_tokens": 4096, "max_input_length": 4096*6},
-    "gpt-3.5-turbo-16k": {"max_tokens": 16384, "max_input_length": 16384*6},
+    "gpt-4": {"min_tokens": 16, "max_tokens": 8192, "max_input_length": 8192*6},
+    "gpt-4-32k": {"min_tokens": 16, "max_tokens": 32768, "max_input_length": 32768*6},
+    "gpt-3.5-turbo": {"min_tokens": 16, "max_tokens": 4096, "max_input_length": 4096*6},
+    "gpt-3.5-turbo-16k": {"min_tokens": 16, "max_tokens": 16384, "max_input_length": 16384*6},
     # text
-    "text-davinci-003": {"max_tokens": 4097, "max_input_length": 4097*6},
-    "text-davinci-002": {"max_tokens": 4097, "max_input_length": 4097*6},
-    "text-davinci-001": {"max_tokens": 4097, "max_input_length": 4097*6},
-    "text-curie-001": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "text-babbage-001": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "text-ada-001": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "davinci": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "curie": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "babbage": {"max_tokens": 2049, "max_input_length": 2049*6},
-    "ada": {"max_tokens": 2049, "max_input_length": 2049*6},
+    "text-davinci-003": {"min_tokens": 16, "max_tokens": 4097, "max_input_length": 4097*6},
+    "text-davinci-002": {"min_tokens": 16, "max_tokens": 4097, "max_input_length": 4097*6},
+    "text-davinci-001": {"min_tokens": 16, "max_tokens": 4097, "max_input_length": 4097*6},
+    "text-curie-001": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "text-babbage-001": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "text-ada-001": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "davinci": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "curie": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "babbage": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
+    "ada": {"min_tokens": 16, "max_tokens": 2049, "max_input_length": 2049*6},
     # code
-    "code-davinci-002": {"max_tokens": 8001, "max_input_length": 8001*6},
-    "code-davinci-001": {"max_tokens": 8001, "max_input_length": 8001*6},
-    "code-cushman-002": {"max_input_length": 8192*6},
-    "code-cushman-001": {"max_input_length": 8192*6},
-    "davinci-codex": {"max_input_length": 8192*6},
-    "cushman-codex": {"max_input_length": 8192*6},
+    "code-davinci-002": {"min_tokens": 16, "max_tokens": 8001, "max_input_length": 8001*6},
+    "code-davinci-001": {"min_tokens": 16, "max_tokens": 8001, "max_input_length": 8001*6},
+    "code-cushman-002": {"min_tokens": 16, "max_tokens": 2048, "max_input_length": 2048*6},
+    "code-cushman-001": {"min_tokens": 16, "max_tokens": 2048, "max_input_length": 2048*6},
+    "davinci-codex": {"min_tokens": 16, "max_input_length": 8192*6},
+    "cushman-codex": {"min_tokens": 16, "max_input_length": 8192*6},
     # edit
-    "text-davinci-edit-001": {"max_input_length": 8192*6},
-    "code-davinci-edit-001": {"max_input_length": 8192*6},
+    "text-davinci-edit-001": {"min_tokens": 16, "max_input_length": 8192*6},
+    "code-davinci-edit-001": {"min_tokens": 16, "max_input_length": 8192*6},
     # embeddings
-    "text-embedding-ada-002": {"max_input_length": 8192*6},
+    # https://learn.microsoft.com/en-us/azure/cognitive-services/openai/concepts/models#embeddings-models-1
+    "text-embedding-ada-002": {"min_tokens": 16, "max_tokens": 8191, "max_input_length": 8191*6},
     # old embeddings
-    "text-similarity-davinci-001": {"max_input_length": 8192*6},
-    "text-similarity-curie-001": {"max_input_length": 8192*6},
-    "text-similarity-babbage-001": {"max_input_length": 8192*6},
-    "text-similarity-ada-001": {"max_input_length": 8192*6},
-    "text-search-davinci-doc-001": {"max_input_length": 8192*6},
-    "text-search-curie-doc-001": {"max_input_length": 8192*6},
-    "text-search-babbage-doc-001": {"max_input_length": 8192*6},
-    "text-search-ada-doc-001": {"max_input_length": 8192*6},
-    "code-search-babbage-code-001": {"max_input_length": 8192*6},
-    "code-search-ada-code-001": {"max_input_length": 8192*6},
+    "text-similarity-davinci-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-similarity-curie-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-similarity-babbage-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-similarity-ada-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-search-davinci-doc-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-search-curie-doc-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-search-babbage-doc-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "text-search-ada-doc-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "code-search-babbage-code-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
+    "code-search-ada-code-001": {"min_tokens": 16, "max_tokens": 2046, "max_input_length": 2046*6},
     # open source
-    "gpt2": {"max_input_length": 8192*6},
+    # "gpt2": {"max_input_length": 8192*6},
 }
 
-def openai_model_metadata(model_name):
+def openai_model_metadata(model_name: str) -> dict:
+    """
+    openai_model_metadata returns the model metadata for an OpenAI model.
+    :param model_name: name of the model to fetch metadata
+    :return: the model's metadata, or {} if the model does not exist
+    """
     if model_name in openai_models_meta.keys():
         return openai_models_meta[model_name]
     elif model_name in model_alias.keys():
         if model_alias[model_name] in openai_models_meta.keys():
             return openai_models_meta[model_alias[model_name]]
-    return None
+    return {}
 
-def openai_token_counts(model_name, input):
+def openai_token_counts(model_name: str, input: str) -> int:
+    """
+    openai_token_counts uses an OpenAI model to calculate token counts from the input string.
+    :param model_name: name of the model used to calculate token counts
+    :param input: the input string to count tokens
+    :return: the token counts as an integer
+    """
     enc = tiktoken.encoding_for_model(model_name)
     ids = enc.encode(input)
     return len(ids)
@@ -76,18 +89,28 @@ hf_cache_dir = "./cache_hf"
 hf_models = {}
 hf_tokenizers = {}
 hf_models_meta = {
-    "sentence-transformers/multi-qa-mpnet-base-cos-v1": {"max_tokens": 512, "max_input_length": 8192},
+    "sentence-transformers/multi-qa-mpnet-base-cos-v1": {"min_tokens": 16, "max_tokens": 512, "max_input_length": 8192},
 }
 
-def hf_model_metadata(model_name):
+def hf_model_metadata(model_name) -> dict:
+    """
+    hf_model_metadata returns the model metadata for an HuggingFace model.
+    :param model_name: name of the model to fetch metadata
+    :return: the model's metadata, or {} if the model does not exist
+    """
     if model_name in hf_models_meta.keys():
         return hf_models_meta[model_name]
     elif model_name in model_alias.keys():
         if model_alias[model_name] in hf_models_meta.keys():
             return hf_models_meta[model_alias[model_name]]
-    return None
+    return {}
 
-def get_model(model_name):
+def hf_get_model(model_name):
+    """
+    hf_get_model returns a HuggingFace model.
+    :param model_name: name of the model to fetch metadata
+    :return: the model, or None if the model does not exist
+    """
     if model_name in hf_models.keys():
         return hf_models[model_name]
     elif model_name in model_alias.keys():
@@ -95,7 +118,12 @@ def get_model(model_name):
             return hf_models[model_alias[model_name]]
     return None
 
-def get_tokenizer(model_name):
+def hf_get_tokenizer(model_name):
+    """
+    hf_get_model returns a HuggingFace tokenizer.
+    :param model_name: name of the model to fetch metadata
+    :return: the tokenizer, or None if the model does not exist
+    """
     if model_name in hf_tokenizers.keys():
         return hf_tokenizers[model_name]
     elif model_name in model_alias.keys():
@@ -103,14 +131,25 @@ def get_tokenizer(model_name):
             return hf_tokenizers[model_alias[model_name]]
     return None
 
-# Mean Pooling - Take average of all tokens
 def mean_pooling(model_output, attention_mask):
+    """
+    mean_pooling calculates average of all tokens.
+    :param model_output:
+    :param attention_mask:
+    :return:
+    """
     token_embeddings = model_output.last_hidden_state #First element of model_output contains all token embeddings
     input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
     return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-# Encode text and return embeddings vector
 def encode_embeddings(model, tokenizer, texts):
+    """
+    encode_embeddings encodes the input text and returns the embedding vector.
+    :param model: the model used to extract embedding vector
+    :param tokenizer: the tokenizer used to tokenize input string
+    :param texts: the input string, or array of string, to extract embedding vector(s)
+    :return: the extracted embedding vector(s)
+    """
     # Tokenize sentences
     encoded_input = tokenizer(texts, padding=True, truncation=True, return_tensors='pt')
     # Compute token embeddings
@@ -121,6 +160,32 @@ def encode_embeddings(model, tokenizer, texts):
     # Normalize embeddings
     embeddings = F.normalize(embeddings, p=2, dim=1)
     return embeddings
+
+#----------------------------------------------------------------------#
+
+def split_text(text: str, length_function, chunk_size: int, chunk_overlap: int = 0) -> [str]:
+    """
+    split_text is utility function to split a long text string into smaller chunks.
+    :param text: the input text to be split
+    :param length_function: function to calculate text length, the function must take in a string and return an int
+    :param chunk_size: maximum chunk size (chunk size is calculated using length_function)
+    :param chunk_overlap: specify how much chunks can overlap with each other
+    :return:
+    """
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    text_splitter = RecursiveCharacterTextSplitter(
+        separators = ["\n\n+", "\n", "[\\.!\\?]\s+", "[,:;]\s+", "\s+", ""],
+        chunk_size = chunk_size,
+        chunk_overlap = chunk_overlap,
+        length_function = length_function,
+    )
+    chunks = text_splitter.split_text(text.replace("\r", "\n"))
+    for i in range(len(chunks)):
+        if i > 0 and re.match("^[.!?,:;]", chunks[i]):
+            chunks[i-1] += chunks[i][0]
+            chunks[i] = chunks[i][1:]
+        chunks[i] = chunks[i].strip()
+    return chunks
 
 # Load models from HuggingFace Hub
 for model_name in hf_models_meta:
