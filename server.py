54d54155ea394538a9e161b35eede63aa7df528c
Thanh Nguyen <thanhn@microsoft.com>
2023-07-06 02:20:03 +1000

    prepare to release v0.3.1

--- server.py
+++ server.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     num_workers = int(os.getenv("NUM_WORKERS", 1))
     if num_workers <= 1:
         num_workers = 1
-        uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=num_workers)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=num_workers)
